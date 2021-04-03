c4b52d1a4b5b66cc2bc2516f4f103ec6368aeb4b
darlandieterich <darlandieterich@gmail.com>
2021-04-03 16:22:37 -0300

    Updated name field

--- manage-file/app/models/directory.rb
+++ manage-file/app/models/directory.rb
@@ -1,4 +1,4 @@
 class Directory < ApplicationRecord
-    has_many :children, :class_name => "Directory", foreign_key: 'parent'
-    belongs_to :parent, :class_name => "Directory", foreign_key: 'parent', :optional => true
-end
+    has_many :children, :class_name => "Directory", foreign_key: 'parent_id'
+    belongs_to :parent, :class_name => "Directory", foreign_key: 'parent_id', :optional => true
+end_id
