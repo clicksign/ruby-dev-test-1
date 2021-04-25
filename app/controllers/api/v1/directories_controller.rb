96f2a1128f76ae5de9f920c8db7e2b6676653668
henrichjones <henrichjones@gmail.com>
2021-04-25 00:03:01 -0300

    Added create method to directories
    - Added directory_id in params permitted,
    - Added new rabl file to show directory object attributes
    
    Signed-off-by: henrichjones <henrichjones@gmail.com>

--- app/controllers/api/v1/directories_controller.rb
+++ app/controllers/api/v1/directories_controller.rb
@@ -1,11 +1,23 @@
 class Api::V1::DirectoriesController < ApplicationController
   def index
-    @directories = Directory.all
+    @directories = Directory.all.where(directory_id: nil)
+  end
+
+  def create
+    respond_to do |format|
+      @directory = Directory.create(directory_params)
+
+      if @directory.save
+        format.json { render 'api/v1/directories/create' } 
+      else
+        render json: @directory.errors.full_messages, status: :unprocessable_entity
+      end
+    end
   end
 
   private
 
   def directory_params
-    params.permit(:name)
+    params.permit(:name, :directory_id)
   end
 end
