50ec2f460cfc281b5175e1ea28bbbcf4f97ad885
henrichjones <henrichjones@gmail.com>
2021-04-26 00:11:47 -0300

    Created archive routes
    Signed-off-by: henrichjones <henrichjones@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :directories
+      resources :directories do
+        resources :archives
+      end
     end
   end
 end
