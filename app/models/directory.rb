44bc7f9c1a5d0769f684f71a803b1066635a1f3c
Lucas Santos da Costa <lsantosc@live.com>
2021-05-31 13:50:13 -0300

    add root directory

--- app/models/directory.rb
+++ app/models/directory.rb
@@ -3,4 +3,8 @@ class Directory < ApplicationRecord
   has_many_attached :files
 
   validates :name, presence: true, uniqueness: { scope: :ancestry }
+
+  before_validation do
+    assign_attributes(ancestry: '1') if ancestry.blank?
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+unless Directory.any?
+  Directory.create(id: 1, name: 'root')
+end
