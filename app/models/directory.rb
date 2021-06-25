02b8b6d8171dc2b853b96483d411f908e0acc140
Bianca <bsquintan@gmail.com>
2021-06-24 21:47:59 -0300

    adds atop_directory association to directories

--- app/models/directory.rb
+++ app/models/directory.rb
@@ -1,4 +1,5 @@
 class Directory < ApplicationRecord
     has_many :archives
-    has_many :subdirectorys, :class_name => "Directory", :foreign_key => 'directory_id'
+    has_many :subdirectories, :class_name => "Directory", :foreign_key => 'directory_id'
+    belongs_to :atop_directory, :class_name => "Directory", :foreign_key => 'directory_id'
 end
