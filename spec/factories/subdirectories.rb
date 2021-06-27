f2c7f61bb90c7fe1cb6950a838aafb86ac00324f
Vítor Ribas <bsbribas66@gmail.com>
2021-06-27 17:52:39 -0300

    test: add specs for subdirectories CRUD

--- spec/factories/subdirectories.rb
+++ spec/factories/subdirectories.rb
@@ -2,5 +2,13 @@ FactoryBot.define do
   factory :subdirectory do
     name { Faker::Lorem.word }
     directory { build(:directory) }
+
+    after :build do |subdirectory|
+      archive = open_archive_example
+      archive2 = open_archive_example
+      subdirectory.archives.attach(io: archive[:file],  filename: archive[:filename])
+      subdirectory.archives.attach(io: archive2[:file], filename: archive2[:filename])
+      subdirectory.save
+    end
   end
 end
