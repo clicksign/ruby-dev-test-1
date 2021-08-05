431921ae61f975b3cdd9972b2e0dbdf17aa100f7
lucas-ideapad <lcppcoding@gmail.com>
2021-08-05 03:38:42 -0300

    no success on renaming strategies. Will probably need to use metaprogramming stuff to hook into ancestry before_save callback and intercept update so we keep track of whats changing. Fun times

--- app/models/directory.rb
+++ app/models/directory.rb
@@ -4,10 +4,21 @@ class Directory < ApplicationRecord
   validates :ancestry, presence: true, if: -> {
     Directory.any?
   }
+  before_save :rename_conflicts
   def name_ancestors(and_self: false)
     if and_self
-      return ancestors.map { |anc| anc.name }.join('/') + "/#{name}"
+      return ancestors.map { |ancestor| ancestor.name }.join('/') + "/#{name}"
     end
-    ancestors.map { |anc| anc.name }.join('/')
+    ancestors.map { |ancestor| ancestor.name }.join('/')
+  end
+
+  private
+
+  def rename_conflicts
+    # Use self for clarity. VERY limited solution.
+    byebug
+    if siblings.where(name: self.name).count == 2
+      self.name = self.name + '(2)'
+    end
   end
 end
