a072b01048280df5b7dc2037a2bef6ee1db5f8ef
Amanda <kimberlypenido@gmail.com>
2021-09-20 21:17:07 -0300

    Add method to store files

--- app/models/document_file.rb
+++ app/models/document_file.rb
@@ -1,4 +1,20 @@
 class DocumentFile < ApplicationRecord
     belongs_to :folder
     has_one_attached :file
+
+    validates :title, presence: { message: "Cannot be blank" }
+
+    
+    def content=(value)
+        unless value.is_a?(String)
+            file = File.basename(value)
+            directory = "#{Rails.root}/public/files"
+            file_directory =  "#{directory}/#{file}"
+
+            Dir.mkdir(directory) unless Dir.exists?(directory)
+            File.open(file_directory, "wb") { |f| f.write(value.read)}
+
+            file_directory
+        end
+    end
 end
