483da820f47173d62d043e4e2cd1cc987f7f2e97
Amanda <kimberlypenido@gmail.com>
2021-09-20 21:16:18 -0300

    Add validation attribute to folder

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -2,4 +2,7 @@ class Folder < ApplicationRecord
     belongs_to :parent, class_name: 'Folder', optional: true
     has_many :sub_folders, class_name: 'Folder', foreign_key: 'parent_id'
     has_many :document_files
+
+    validates :title, presence: { message: "Cannot be blank" }
+    validates :title, uniqueness: { message: "This folder name already exist" }
 end
