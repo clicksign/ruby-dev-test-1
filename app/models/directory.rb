ba06d7b9bcb436a07cb8793b2360dfb1cf3674b5
franciscognclvs <fgp1991@gmail.com>
2021-10-25 18:46:25 -0300

    added validations

--- app/models/directory.rb
+++ app/models/directory.rb
@@ -2,4 +2,7 @@ class Directory < ApplicationRecord
   belongs_to :parent, class_name: 'Directory', optional: true
   has_many :subdirectory, class_name: 'Directory', foreign_key: 'parent_id'
   has_many :documents
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- app/models/document.rb
+++ app/models/document.rb
@@ -1,2 +1,6 @@
 class Document < ApplicationRecord
+  belongs_to :directory
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
