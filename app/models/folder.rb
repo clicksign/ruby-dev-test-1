8335c17ff94ce209b27bee7aba00647bad73ba20
Jonathan Guarnieri <djou.guarnieri@hotmail.com>
2021-10-26 20:33:18 -0300

    fix the Folder model, adding a foreign_key to folders association

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -2,6 +2,6 @@ class Folder < ApplicationRecord
   belongs_to :parent, class_name: 'Folder',
                       foreign_key: 'parent_id',
                       required: false
-  has_many :folders
+  has_many :folders, foreign_key: 'parent_id'
   has_many_attached :archives
 end
