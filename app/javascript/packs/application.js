65451e219dbcd6c5337f483e6ff299b7bca502a2
kandebonfim <kandebonfim@gmail.com>
2021-11-08 13:35:05 -0300

    Implement frontend basics

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+document.addEventListener("DOMContentLoaded",function(){
+  feather.replace()
+});
