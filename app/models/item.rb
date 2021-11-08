422615dcc9ec4a5ab0f2456dfda24afaecfba252
kandebonfim <kandebonfim@gmail.com>
2021-11-08 12:32:16 -0300

    Update Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ApplicationRecord
   belongs_to :folder
+  has_one_attached :file
 end
--- db/migrate/20211108134728_create_items.rb
+++ db/migrate/20211108134728_create_items.rb
@@ -2,7 +2,6 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
       t.string :name
-      t.binary :data, :limit => 1.megabyte
       t.references :folder, foreign_key: true
 
       t.timestamps
