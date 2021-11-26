c725968fba103583f715cff52f6cda54771860d3
Heitor <heitorbelem21@gmail.com>
2021-11-26 17:07:34 -0300

    Add test cases to folder model

--- test/models/folder_test.rb
+++ test/models/folder_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class FolderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should save folder with valid attributes" do
+    assert folder = Folder.create(name: "Documentos")
+  end
+  
+  test "should not save a folder without name" do
+    parent_folder = Folder.create(name: "Documentos")
+    folder = Folder.create(parent_folder_id: parent_folder.id)
+    assert_not folder.save
+  end
+
 end
