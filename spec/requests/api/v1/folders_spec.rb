42717e03b66dd111bb615e92e4ea18182f74cf19
Heitor <heitorbelem21@gmail.com>
2021-11-27 11:18:46 -0300

    Add more tests

--- spec/requests/api/v1/folders_spec.rb
+++ spec/requests/api/v1/folders_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Handle folders' do
   
-  describe "get all folders", type: :request do 
+  describe 'get all folders', type: :request do 
     let!(:folders) { FactoryBot.create_list(:folder, 10) }
     
     before { get '/api/v1/folders' }
@@ -16,7 +16,7 @@ RSpec.describe 'Handle folders' do
     end
   end
 
-  describe "show specific folder", type: :request do
+  describe 'show folder', type: :request do
     let!(:requested_folder) { FactoryBot.create(:folder) }
 
     before { get "/api/v1/folders/#{requested_folder.id}" }
@@ -59,9 +59,35 @@ RSpec.describe 'Handle folders' do
     end
 
     describe "move folder" do
-      pending
-    end
+      let!(:new_folder) { FactoryBot.create(:folder) }
+      before do
+        put(
+          "/api/v1/folders/#{requested_folder.id}",
+          params: {
+            folder: {
+              parent_folder_id: new_folder.id
+            }
+          }
+        )
+      end
+      
+      it 'should move to new parent folder' do
+        expect(JSON.parse(response.body)["parent_folder_id"]).to eq(new_folder.id)
+      end
 
+      it 'should return http status 200' do
+        expect(response).to 
+      end
+    end
+  end
+
+  describe "delete folder" do    
+    it "destroys the requested folder" do
+      folder = Folder.create!(name: "Test")
+      expect { 
+        delete "/api/v1/folders/#{folder.id}"
+      }.to change(Folder, :count).by(-1)
+    end
   end
 
 end
