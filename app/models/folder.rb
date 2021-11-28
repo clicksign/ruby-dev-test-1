71e6f43c27d2f7edc7d3947fce54d2be8a11fa3c
Alfredo Del Fabro Neto <alfredodfn@gmail.com>
2021-11-28 12:49:32 -0300

    Adds validation to prevent create docs with same filename

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -7,12 +7,13 @@ class Folder < ApplicationRecord
 
   validates :name, presence: true
   validates :name, uniqueness: { scope: :parent_id }
+  validate :document_uniquiness
   
   before_destroy :ensure_is_not_root, prepend: true do
     throw(:abort) if errors.present?
   end
 
-  before_create :ensure_uniqueness_of_root, prepend: true do
+  before_create :ensure_document_name_is_unique, prepend: true do
     throw(:abort) if errors.present?
   end
 
@@ -22,11 +23,16 @@ class Folder < ApplicationRecord
   private
 
   def ensure_is_not_root
-    errors.add(:base, "Root folder cannot be removed!") if self.parent_id.nil?
+    errors.add(:base, 'Root folder cannot be removed!') if self.parent_id.nil?
   end
 
   def ensure_uniqueness_of_root
-    errors.add(:base, "There is only one root folder!") if Folder.where(parent_id: nil).size > 0
+    errors.add(:base, 'There is only one root folder!') if Folder.where(parent_id: nil).size > 0
+  end
+
+  def document_uniquiness
+    array_of_filenames = self.documents.map{|doc| doc.blob.filename.to_s }
+    errors.add(:documents, 'Document already exists!') if array_of_filenames.uniq.length != array_of_filenames.length
   end
 
 end
