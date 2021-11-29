07d8cb57f12c620ba2f76e0f11aed0cc6f43f511
Heitor <heitorbelem21@gmail.com>
2021-11-29 12:27:41 -0300

    🎨 Add folder controller actions

--- app/controllers/api/v1/folders_controller.rb
+++ app/controllers/api/v1/folders_controller.rb
@@ -1,2 +1,47 @@
 class Api::V1::FoldersController < ApplicationController
+    before_action :set_folder, only: [:show, :update, :destroy]
+
+    def index
+        @folders = Folder.all
+        render json: @folders
+    end
+
+    def show
+        render json: @folder
+    end
+
+    def create
+        @folder = Folder.new(folder_params)
+
+        unless @folder.save
+            render json: @folder.errors, status: :unprocessable_entity
+        end
+
+        render json: @folder, status: :created
+    end
+
+    def update
+        unless @folder.update(folder_params)
+            render json: @folder.errors, status: :unprocessable_entity
+        end
+        render json: @folder
+    end
+
+    def destroy
+        unless @folder.destroy
+            render json: @folder.errors, status: :unprocessable_entity
+        end
+        render json: nil, status: :no_content
+    end
+
+    private
+
+    def folder_params
+        params.require(:folder).permit(:name, :parent_folder_id, files: [])
+    end
+
+    def set_folder
+        @folder = Folder.find(params[:id])
+    end
+
 end
