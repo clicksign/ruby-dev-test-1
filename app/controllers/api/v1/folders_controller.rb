d41345cd7f66463026caa004a3a3df5b60e451c5
Heitor <heitorbelem21@gmail.com>
2021-11-29 15:38:54 -0300

    ✨ Create route for file upload

--- app/controllers/api/v1/folders_controller.rb
+++ app/controllers/api/v1/folders_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::FoldersController < ApplicationController
-    before_action :set_folder, only: [:show, :update, :destroy]
+    before_action :set_folder, except: [:index, :create]
 
     def index
         @folders = Folder.all
@@ -12,11 +12,9 @@ class Api::V1::FoldersController < ApplicationController
 
     def create
         @folder = Folder.new(folder_params)
-
         unless @folder.save
             render json: @folder.errors, status: :unprocessable_entity
         end
-
         render json: @folder, status: :created
     end
 
@@ -30,15 +28,22 @@ class Api::V1::FoldersController < ApplicationController
     def destroy
         @folder.destroy
     end
+    
+    def upload_files
+        @folder.files.attach(file_upload_params[:files])
+    end
 
     private
 
     def folder_params
-        params.require(:folder).permit(:name, :parent_folder_id, files: [])
+        params.permit(:name, :parent_folder_id)
     end
 
     def set_folder
-        @folder = Folder.find(params[:id])
+        @folder = Folder.find(params[:id] || params[:folder_id])
     end
 
+    def file_upload_params
+        params.permit(files: [])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
-      resources :folders
+      resources :folders do
+        post "/files", to: "/api/v1/folders#upload_files"
+      end
     end
   end
 end
