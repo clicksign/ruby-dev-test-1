84a8d042b3eb7d5d92d426cdc9a167bcd56989de
evandrotvc <evandro.castro@cjr.org.br>
2022-01-08 16:19:44 -0300

    🧪  Add tests about create Folders

--- test/models/folder_test.rb
+++ test/models/folder_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class FolderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save folder with valid attributes" do
+    assert folder = Folder.create(name: "Workspace")
+  end
+
+  test "should save a folder with parent folder" do
+    parent_folder = Folder.create(name: "Workspace")
+    folder = Folder.create(name: "ReactJs" ,parent_folder_id: parent_folder.id)
+    assert folder.save
+  end
+
+  test "should not save a folder without name" do
+    parent_folder = Folder.create(name: "Workspace")
+    folder = Folder.create(parent_folder_id: parent_folder.id)
+    assert_not folder.save
+  end
 end
