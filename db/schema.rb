ea842208b7897dfbbcecf59d67ec25ab6f46a45d
evandrotvc <evandro.castro@cjr.org.br>
2022-01-08 10:11:55 -0300

    💾 Add pgcrypto in the project

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_07_004955) do
+ActiveRecord::Schema.define(version: 2022_01_07_221252) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
