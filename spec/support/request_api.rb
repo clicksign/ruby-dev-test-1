7be3e7ee19fbe35ea93e7790763f34659270ba74
Emerson Freitas <mersonsi@gmail.com>
2022-01-10 19:46:54 -0300

    setup: update schema and add helpers

---
+++ spec/support/request_api.rb
@@ -0,0 +1,21 @@
+module RequestAPI
+
+  def body_json(symbolize_keys: false)
+    json = JSON.parse(response.body)
+    symbolize_keys ? json.deep_symbolize_keys : json
+  rescue
+    return {}
+  end
+end
+
+def auth_header(merge_with: {})
+  header = {
+              'Content-Type' => 'application/json',
+              'Accept' => 'application/json'
+            }
+  header.merge(merge_with)
+end
+
+RSpec.configure do |config|
+  config.include RequestAPI, type: :request
+end
