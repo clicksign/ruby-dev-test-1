2231386cfdf2d3772b21e89f1b2f112d98d09748
Arlisson Jaques <arlissonjaques@gmail.com>
2022-02-15 20:21:25 -0300

    rubocop -A

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Folder < ApplicationRecord
   has_many :subfolders, class_name: 'Folder', foreign_key: 'ref_id'
   belongs_to :ref, class_name: 'Folder', optional: true
