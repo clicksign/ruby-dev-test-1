b326baa07656110eba4b15c191087c2eba782509
Arlisson Jaques <arlissonjaques@gmail.com>
2022-02-15 20:43:30 -0300

    add dependency for destruction

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class Folder < ApplicationRecord
-  has_many :subfolders, class_name: 'Folder', foreign_key: 'ref_id'
+  has_many :subfolders, class_name: 'Folder',
+                        foreign_key: 'ref_id', dependent: :delete_all
   belongs_to :ref, class_name: 'Folder', optional: true
 
   validates_presence_of :name
