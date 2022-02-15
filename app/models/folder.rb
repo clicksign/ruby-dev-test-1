9967ed605b93014ab4e1e88b183d10e5726bc640
Arlisson Jaques <arlissonjaques@gmail.com>
2022-02-15 20:23:33 -0300

    add validation

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -3,4 +3,6 @@
 class Folder < ApplicationRecord
   has_many :subfolders, class_name: 'Folder', foreign_key: 'ref_id'
   belongs_to :ref, class_name: 'Folder', optional: true
+
+  validates_presence_of :name
 end
