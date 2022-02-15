1510f550cf56cd154359d48aaf34667d42cc2dee
Arlisson Jaques <arlissonjaques@gmail.com>
2022-02-15 20:17:28 -0300

    fix the relationship name

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,4 +1,4 @@
 class Folder < ApplicationRecord
   has_many :subfolders, class_name: 'Folder', foreign_key: 'ref_id'
-  belogs_to :ref, class_name: 'Folder', optional: true
+  belongs_to :ref, class_name: 'Folder', optional: true
 end
