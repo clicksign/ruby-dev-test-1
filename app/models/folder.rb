2e947bfeb793d1cfbc9310e2f2eae2b5752409df
Arlisson Jaques <arlissonjaques@gmail.com>
2022-02-15 20:15:27 -0300

    fix relationships and use ref_id like :foreign_key

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,4 +1,4 @@
 class Folder < ApplicationRecord
-  has_many :subfolders, class_name: 'Folder'
-  belogs_to :ref
+  has_many :subfolders, class_name: 'Folder', foreign_key: 'ref_id'
+  belogs_to :ref, class_name: 'Folder', optional: true
 end
