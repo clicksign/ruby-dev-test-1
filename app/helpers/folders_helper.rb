b211818c4335cf259e957203e7bb76c739912e3c
Carlos Andrade <krlosgilson@gmail.com>
2022-03-17 21:34:05 -0300

    Create a helper to get the parent folder link

--- app/helpers/folders_helper.rb
+++ app/helpers/folders_helper.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
+# FoldersHelper
 module FoldersHelper
+  def link_to_parent_folder(folder)
+    folder.parent_folder.present? ? folder_path(folder.parent_folder) : folders_path
+  end
 end
