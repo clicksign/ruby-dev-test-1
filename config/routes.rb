c484aff06660e1988b1c4af355d45d99bae107c0
Lucas Araujo <lucas.trabalho.araujo@gmail.com>
2022-05-15 02:27:11 -0300

    refactor: update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :directories
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :directories do
+        member do
+          post :upload_file
+        end
+      end
+    end
+  end
 end
