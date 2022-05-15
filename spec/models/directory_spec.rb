e3bc0c03a1ed50f2ac2807baf22353c77e750ec9
Lucas Araujo <lucas.trabalho.araujo@gmail.com>
2022-05-15 01:00:45 -0300

    test: add directory_spec.rb

--- spec/models/directory_spec.rb
+++ spec/models/directory_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
+include ActiveModel::Model
 
 RSpec.describe Directory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Associations" do
+    it { should belong_to(:parent).without_validating_presence }
+    it { should have_many(:subdirectories).dependent(:destroy) }
+  end
+
+  describe "Validations" do
+    it { should validate_presence_of(:title) }
+  end
+
+  it "has many files" do
+    expect(subject.files).to be_an_instance_of(
+                               ActiveStorage::Attached::Many
+                             )
+  end
 end
