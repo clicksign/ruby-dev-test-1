7715c28e9a47f796604d959ca18b7cd2c584f85e
Lucas Araujo <lucas.trabalho.araujo@gmail.com>
2022-05-15 00:18:48 -0300

    refactor(app/models): add validates in directory.rb

--- app/models/directory.rb
+++ app/models/directory.rb
@@ -5,4 +5,7 @@ class Directory < ApplicationRecord
            class_name: 'Directory', foreign_key: 'parent_id'
 
   has_many_attached :files
+
+  validates :title, presence: true
+  validates_uniqueness_of :title, scope: :parent_id
 end
