b41c0013ff9ac40e4bb94f67a783a4ed7675b058
Erick Pereira de Oliveira <erick.pereira86@gmail.com>
2022-05-22 23:49:05 -0400

    Adicionando remoção de pasta

--- app/models/app_folder.rb
+++ app/models/app_folder.rb
@@ -1,14 +1,22 @@
 class AppFolder < ApplicationRecord
   has_many :children, class_name: 'AppFolder', foreign_key: 'parent_id'
+  has_many :app_files
   belongs_to :parent, class_name: 'AppFolder', foreign_key: 'parent_id', optional: true
 
   validates :folder_name, presence: true
 
   scope :root_folders, -> { where(parent_id: nil) }
 
+  before_destroy :remove_children
+
   def root_folder
     return self unless parent.present?
 
     parent.root_folder
+  end
+
+  def remove_children
+    children.destroy_all
+    app_files.destroy_all
   end
 end
