287b7a0e408f6f91058e3aa1ef316120414108cb
Geraldo Júnior <junior.bimbo@gmail.com>
2022-06-03 01:04:10 -0300

    feat: add toastr notification

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,31 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "popper"
+// import "popper"
 import "@fortawesome/fontawesome-free"
 import * as bootstrap from "bootstrap"
+
+import toastr from "toastr";
+
+document.addEventListener('DOMContentLoaded', () => {
+  toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": true,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
+
+  JSON.parse(document.body.dataset.flashMessages).forEach(flashMessage => {
+    const [type, message] = flashMessage
+    toastr[(type == 'alert') ? 'error' : 'success'](message)
+  })
+});
--- config/importmap.rb
+++ config/importmap.rb
@@ -3,3 +3,5 @@
 pin "application", preload: true
 pin 'bootstrap', to: 'https://cdn.skypack.dev/bootstrap'
 pin "@fortawesome/fontawesome-free", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.1.1/js/all.js"
+pin "toastr", to: "https://ga.jspm.io/npm:toastr@2.1.4/toastr.js"
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js"
