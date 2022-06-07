44f9cf17d3847efa6099fe792386bcff0daf9b8a
Hugo Silva <hugocardososilva@gmail.com>
2022-06-07 02:35:25 -0300

    refactor(folder): change has many options

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,4 +1,4 @@
 class Folder < ApplicationRecord
-  belongs_to :parent, class_name: 'Folder'
-  has_many :children, class_name: 'Folder', foreign_key: 'parent_id'
+  belongs_to :parent, class_name: 'Folder' , optional: true
+  has_many :sub_folders, class_name: 'Folder', foreign_key: 'parent_id', dependent: :destroy
 end
