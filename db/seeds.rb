02ce0f4fdaa442674af3fd377596cd6376a92f47
Jafer Daltro <jafer.daltro@gmail.com>
2022-06-08 17:11:50 -0300

    configure seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+my_doc = Directory.create(name: "my documents")
+videos = Directory.create(name: "videos")
+desktop = Directory.create(name: "Desktop")
+
+my_doc.child_binds.create(child_id: videos.id)
+my_doc.child_binds.create(child_id: desktop)
+
+videos.archives.create(name: "test_file_1")
+videos.archives.create(name: "test_file_2")
+videos.archives.create(name: "test_file_3")
