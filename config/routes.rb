e010f97ef66fb919943071c7ad74c3a69ccd22ef
Jafer <jafer.daltro@gmail.com>
2022-06-08 18:37:46 -0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  root 'v1/directories#index'
   namespace :v1 do
-    root 'directories#index'
-    resources :directories
+    resources :directories, only: %i(index create)
     resources :archives, only: %i(create)
   end
 end
