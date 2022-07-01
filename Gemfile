b00f5154efb94218babcdd436a3b04b0dd6cc56a
Renan Machado <renanmacs@gmail.com>
2022-07-01 13:47:46 -0300

    Add simplecov and factory_bot

--- Gemfile
+++ Gemfile
@@ -5,13 +5,14 @@ ruby '3.1.2'
 
 gem 'rails', '~> 7.0.3'
 
+gem 'bootsnap', require: false
 gem 'pg', '~> 1.1'
 gem 'puma', '~> 5.0'
-gem 'bootsnap', require: false
 
 group :development, :test do
   gem 'byebug'
   gem 'rspec-rails'
+  gem 'factory_bot_rails'
 end
 
 group :development do
@@ -20,3 +21,6 @@ group :development do
   gem 'rubocop-rspec'
 end
 
+group :test do
+  gem 'simplecov', require: false
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,6 @@
+require 'simplecov'
+SimpleCov.start
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -5,6 +8,7 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'support/factory_bot'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -32,7 +36,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  #config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
