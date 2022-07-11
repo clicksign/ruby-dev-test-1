7673c720121a999a4cf15cd2d5fb88f2a7992f6a
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-07-10 23:18:19 -0300

    Cria método para conversão de valores em boleanos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 Dir["services/*.rb"].each { |file| require file }
 
 class ApplicationController < ActionController::API
+  private
+
+  def parse_boolean(value)
+    ActiveRecord::Type::Boolean.new.deserialize(value)
+  end
 end
