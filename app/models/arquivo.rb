40d2cb5ff999989b34379445c3335a25f84fccba
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-07-11 19:20:38 -0300

    Cria validações básicas para o arquivo

--- app/models/arquivo.rb
+++ app/models/arquivo.rb
@@ -5,11 +5,33 @@ class Arquivo < ActiveRecord::Base
   belongs_to :diretorio, class_name: 'Arquivo', optional: true
   scope :diretorios_raiz, -> { where(diretorio_id: nil) }
 
+  validate :conteudo_obrigatorio, :conteudo_nao_obriigatorio, :diretorio_invalido
+
   def pasta?
     pasta === true
   end
 
   def caminho_completo
     caminho == '/' ? "/#{nome}" : caminho
+  end
+
+  private
+
+  def conteudo_obrigatorio
+    if conteudo.blank? && !pasta?
+      errors.add(:conteudo, "O conteúdo do arquivo é obrigatório")
+    end
+  end
+
+  def conteudo_nao_obriigatorio
+    if !conteudo.blank? && pasta?
+      errors.add(:conteudo, "Pasta não pode ter conteúdo")
+    end
+  end
+
+  def diretorio_invalido
+    if !diretorio.nil? && diretorio.pasta?
+      errors.add(:conteudo, "O diretório informado não é uma pasta")
+    end
   end
 end
