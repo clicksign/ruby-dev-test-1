3666b53244bddaa255e0dc2ff2d54ec03958b0bd
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-07-12 03:05:02 -0300

    Configura o rspec e a gem de limpeza do banco

---
+++ spec/rails_helper.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+
+require File.expand_path('../config/environment', __dir__)
+
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'rspec/rails'
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+  config.include FactoryBot::Syntax::Methods
+  # config.include Requests::JsonHelpers, type: :request
+  config.before(:suite) do
+    DatabaseCleaner.allow_remote_database_url = true
+    DatabaseCleaner.clean_with(:deletion)
+    DatabaseCleaner.strategy = :transaction
+  end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+  config.after(:each) do
+    ActiveStorage::Attachment.all.each { |attachment| attachment.purge }
+    DatabaseCleaner.clean
+  end
+end
