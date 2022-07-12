cea431fa272a8e68cca31d64f8229d40a9ab9af5
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-07-12 19:08:16 -0300

    Ajusta busca

--- app/services/atualizar_arquivo_service.rb
+++ app/services/atualizar_arquivo_service.rb
@@ -1,7 +1,7 @@
 class AtualizarArquivoService
   def mover(arquivo_id, params)
     arquivo = Arquivo.find(arquivo_id)
-    arquivo.diretorio = Arquivo.find_by_id(params[:diretorio])
+    arquivo.diretorio = Arquivo.find(params[:diretorio]) unless params[:diretorio].blank?
     arquivo.save
     arquivo
   end
--- app/services/criar_arquivo_service.rb
+++ app/services/criar_arquivo_service.rb
@@ -1,7 +1,7 @@
 class CriarArquivoService
   def create(params)
     arquivo = Arquivo.new({ nome: params[:nome], pasta: params[:pasta]})
-    arquivo.diretorio = Arquivo.find_by_id(params[:diretorio])
+    arquivo.diretorio = Arquivo.find(params[:diretorio]) unless params[:diretorio].blank?
 
     formatar_nome_arquivo(arquivo)
     anexar_arquivo(arquivo, params[:conteudo])
@@ -13,11 +13,12 @@ class CriarArquivoService
   private
 
   def formatar_nome_arquivo(arquivo)
-    arquivo.nome = ActiveStorage::Filename.new(arquivo.nome).sanitized
+    arquivo.nome = ActiveStorage::Filename.new(arquivo.nome).sanitized unless arquivo.nome.nil?
   end
 
   def anexar_arquivo(arquivo, conteudo)
     return if conteudo.nil? || conteudo.blank?
+    return if arquivo.nome.nil? || arquivo.nome.blank?
 
     conteudo_decodificado = conteudo_decodificado(conteudo, arquivo.nome)
     arquivo.conteudo.attach(conteudo_decodificado)
