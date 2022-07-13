7ef659221adc4f52eb72d8b337add3b925139851
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-07-12 21:56:35 -0300

    Cria helper para tratamento do JSON

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,11 @@
-require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'spec_helper'
+require 'support/request_helper'
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
@@ -15,11 +16,10 @@ end
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  # config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.include FactoryBot::Syntax::Methods
-  # config.include Requests::JsonHelpers, type: :request
+  config.include Requests::JsonHelpers, :type => :controller
   config.before(:suite) do
     DatabaseCleaner.allow_remote_database_url = true
     DatabaseCleaner.clean_with(:deletion)
