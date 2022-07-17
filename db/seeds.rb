633f96431cda34a30bce94a9603fd975e419e9fa
ItaloMennaSilveira <italo.silveira@nave.rs>
2022-07-17 02:04:18 -0300

    chore: create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+folder1 = Folder.create(name: 'folder1_with_content')
+
+folder2 = Folder.create(name: 'folder2_with_files')
+
+folder3 = Folder.create(name: 'folder3_empty')
+
+folder4 = Folder.create(name: 'folder4_empty')
+
+archive = Archive.create(name: 'file1')
+archive.file.attach(io: File.open('spec/fixtures/lorem-ipsum.pdf'), filename: 'lorem-ipsum.pdf')
+
+archive2 = Archive.create(name: 'file2')
+archive2.file.attach(io: File.open('spec/fixtures/lorem-ipsum.pdf'), filename: 'lorem-ipsum.pdf')
+
+folder5 = Folder.create(name: 'folder5_with_content', parent: folder1)
+folder6 = Folder.create(name: 'folder6_empty', parent: folder1)
+
+archive3 = Archive.create(name: 'file3', folder: folder1)
+archive3.file.attach(io: File.open('spec/fixtures/lorem-ipsum.pdf'), filename: 'lorem-ipsum.pdf')
+
+archive4 = Archive.create(name: 'file4', folder: folder1)
+archive4.file.attach(io: File.open('spec/fixtures/lorem-ipsum.pdf'), filename: 'lorem-ipsum.pdf')
+
+folder7 = Folder.create(name: 'folder7', parent: folder5)
+
+archive5 = Archive.create(name: 'file5', folder: folder5)
+archive5.file.attach(io: File.open('spec/fixtures/lorem-ipsum.pdf'), filename: 'lorem-ipsum.pdf')
+
+archive6 = Archive.create(name: 'file6', folder: folder5)
+archive6.file.attach(io: File.open('spec/fixtures/lorem-ipsum.pdf'), filename: 'lorem-ipsum.pdf')
+
+folder8 = Folder.create(name: 'folder8', parent: folder7)
+
+archive7 = Archive.create(name: 'file7', folder: folder7)
+archive7.file.attach(io: File.open('spec/fixtures/lorem-ipsum.pdf'), filename: 'lorem-ipsum.pdf')
+
+archive8 = Archive.create(name: 'file8', folder: folder2)
+archive8.file.attach(io: File.open('spec/fixtures/lorem-ipsum.pdf'), filename: 'lorem-ipsum.pdf')
+
+archive9 = Archive.create(name: 'file9', folder: folder2)
+archive9.file.attach(io: File.open('spec/fixtures/lorem-ipsum.pdf'), filename: 'lorem-ipsum.pdf')
