c328b075b500f82b666bf3b745c9bbd0dcdfe227
Allan Escóssio <allanjohnson.pe@gmail.com>
2022-09-01 19:12:16 -0300

    chore: Add application custom conf

--- config/application.rb
+++ config/application.rb
@@ -12,28 +12,18 @@ require "action_mailbox/engine"
 require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module FileStorage
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
+    config.time_zone = "Brasilia"
+    I18n.default_locale = :"pt-BR"
+    config.active_record.schema_format = :sql
     config.api_only = true
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
