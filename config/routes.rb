b5a1e90a0b77061ebb402f61a9982514ab24e243
Tiago Alves <tiago_lima411@hotmail.com>
2022-09-09 09:34:05 -0300

    Aponta root path para folders

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :documents
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'documents#index'
+  root to: 'folders#index'
+
+  # Folders
+  resources :folders
 end
