fd8a9ed1d3d6070c53eaa4e31564b3630f3f78f2
Tiago Alves <tiago_lima411@hotmail.com>
2022-09-11 16:34:53 -0300

    Adiciona navegação em pastas no side menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,43 @@
 module ApplicationHelper
+  def list_folders(folder)
+    return unless user_signed_in?
+
+    "<ul>
+      <li><span class='caret'>#{folder.name}</span>
+        <ul class='nested'>".html_safe +
+          get_children(folder).html_safe  +
+        "</ul>
+      </li>
+    </ul>".html_safe
+  end
+
+  def get_children(parent)
+    children = []
+    parent.children.each do |child|
+      children << "<li><a href='/folders/#{child.id}' >#{child.name}</a></li>"
+    end
+
+    children.join
+  end
+
+  def list_documents(folder)
+    return unless user_signed_in?
+
+    "<ul>
+      <li><span class='caret'>#{folder.name}</span>
+        <ul class='nested'>".html_safe +
+          get_docs(folder).html_safe  +
+        "</ul>
+      </li>
+    </ul>".html_safe
+  end
+
+  def get_docs(folder)
+    docs = []
+    folder.documents.each do |doc|
+      docs << "<li><span>#{doc.file.blob.filename}</span></li>"
+    end
+
+    docs.join
+  end
 end
