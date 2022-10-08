b51f8c17b58ba43ded657c530ef6eb5965181377
Pedro Gabriel <eeevee@gmail.com>
2022-10-08 19:55:20 -0300

    chore: add attached files to folder model

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,6 +1,7 @@
 class Folder < ApplicationRecord
   belongs_to :parent_folder, class_name: "Folder", optional: true
   has_many :folders, class_name: "Folder", foreign_key: "parent_folder_id"
+  has_many_attached :files
 
   validates_presence_of :name
 end
