5e3b2e6f2fe51a0dab62ee6ef9952fd4bc76ef72
Jum Iwamoto <iwamoto.jum@gmail.com>
2022-10-12 19:44:47 -0300

    remove callback desnecessario

--- app/models/directory.rb
+++ app/models/directory.rb
@@ -1,11 +1,10 @@
 class Directory < ApplicationRecord
   belongs_to :parent_directory, class_name: 'Directory', optional: true
-  has_many :sub_directories, class_name: 'Directory', foreign_key: 'parent_directory_id'
+  has_many :sub_directories, class_name: 'Directory', foreign_key: 'parent_directory_id', dependent: :destroy
 
-  has_many :image_files
+  has_many :image_files, dependent: :destroy
 
   after_create :add_default_name
-  after_destroy :destroy_related
 
   def directory_informations
     (self.sub_directories.pluck(:name) + self.image_files.pluck(:name)).join(' + ')
@@ -15,10 +14,5 @@ class Directory < ApplicationRecord
 
   def add_default_name
     self.update(name: "directory_#{self.id}")
-  end
-
-  def destroy_related
-    self.sub_directories.destroy_all if self.sub_directories.any?
-    self.image_files.destroy_all if self.image_files.any?
   end
 end
