f1cad76f29419a765824350080b23200c2df6745
Rafael Santos <santos.rafaelmedeiros@gmail.com>
2022-10-14 14:35:57 -0300

    Rails installation and Gem dependencies

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActionController::ParameterMissing, with: :missing_param
+
+  private
+
+  def record_not_found(e)
+    render json: { error: e.to_s }, status: :not_found
+  end
+
+  def missing_param(e)
+    render json: { error: e.to_s }, status: :bad_request
+  end
+end
