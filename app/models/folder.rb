fb4dad3bbc381d8513a2ec11d3badd892d050449
Andrey Thaywry <thaywry50@gmail.com>
2022-10-17 04:38:08 -0300

    rename children_folders to child_folders

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,6 +1,6 @@
 class Folder < ApplicationRecord
   belongs_to :parent, class_name: "Folder", optional: true
-  has_many :children_folders, class_name: "Folder", foreign_key: :parent_id
+  has_many :child_folders, class_name: "Folder", foreign_key: :parent_id
 
   validates :name, presence: true, uniqueness: { scope: :parent }
 end
