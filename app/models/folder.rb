87422f3786548d544f8d409aab850eeb7f9884f8
Horecio Araujo Dias <horecio@gmail.com>
2022-10-24 14:58:18 +0000

    feat(folder): add validation uniqueness to label scope with ancestry

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,3 +1,6 @@
 class Folder < ApplicationRecord
   validates :label, presence: true
+  validates :label, uniqueness: { scope: :ancestry, case_sensitive: true }
+
+  has_ancestry
 end
--- spec/models/folder_spec.rb
+++ spec/models/folder_spec.rb
@@ -4,4 +4,20 @@ RSpec.describe Folder, type: :model do
   context 'validations' do
     specify { is_expected.to validate_presence_of(:label) }
   end
+
+  describe 'uniqueness index label scoped with ancestry' do
+    specify { is_expected.to have_db_column(:ancestry).of_type(:string) }
+
+    context 'if ancestry is null' do
+      before { allow(subject).to receive(:ancestry).and_return(nil) }
+
+      specify { is_expected.to have_db_index(:label).unique(true) }
+    end
+
+    context 'if ancestry is not null' do
+      before { allow(subject).to receive(:ancestry).and_return('1/2') }
+
+      specify { is_expected.to have_db_index(%i[label ancestry]).unique(true) }
+    end
+  end
 end
