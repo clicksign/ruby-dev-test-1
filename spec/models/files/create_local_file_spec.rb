cd10b15b9b33760af3f609f5bebc5dea2122bcd4
Horecio Araujo Dias <horecio@gmail.com>
2022-10-25 15:02:20 +0000

    test(local_file): Add failure tests

--- spec/models/files/create_local_file_spec.rb
+++ spec/models/files/create_local_file_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Files::CreateLocalFile do
-  context 'when params are valid' do
-    describe '.save' do
+
+  describe '.save' do
+    context 'when params are valid' do
       specify do
         folder = create(:folder)
         params = attributes_for(:local_file, folder_id: folder.id)
@@ -12,7 +13,39 @@ RSpec.describe Files::CreateLocalFile do
         expect do
           creator.save
         end.to change(LocalFile, :count).by(1)
-           .and change(Medium, :count).by(1)
+          .and change(Medium, :count).by(1)
+      end
+    end
+
+    context 'when folder not exists' do
+      specify do
+        folder = create(:folder)
+        params = attributes_for(:local_file, folder_id: folder.id)
+        folder.destroy
+
+        creator = described_class.new(params:)
+
+        expect do
+          creator.save
+        end.to raise_error(ActiveRecord::RecordInvalid)
+          .and change(LocalFile, :count).by(0)
+          .and change(Medium, :count).by(0)
+      end
+    end
+
+    context 'when file name is nil' do
+      specify do
+        folder = create(:folder)
+        params = attributes_for(:local_file, name: nil, folder_id: folder.id)
+        folder.destroy
+
+        creator = described_class.new(params:)
+
+        expect do
+          creator.save
+        end.to raise_error(ActiveRecord::RecordInvalid)
+          .and change(LocalFile, :count).by(0)
+          .and change(Medium, :count).by(0)
       end
     end
   end
