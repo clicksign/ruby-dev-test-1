e191cb015b5f7adf6e9d7181f4d9e33d9c585dc8
Felipe Rubio <felipe.carvalho.rubio@gmail.com>
2022-11-10 01:23:56 -0300

    chore: add relationships and validations to directories and files

--- app/models/file.rb
+++ app/models/file.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 class File < ApplicationRecord
   belongs_to :directory
+  has_one_attached :content
+
+  validates :name, :content, :directory, presence: true
+  validates_uniqueness_of :name, scope: :directory_id
 end
