f221e0fabaad300aa9a3382b8d0b6438d0d37072
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2022-12-04 17:37:42 -0500

    Add sub_directories

--- app/models/file_upload.rb
+++ app/models/file_upload.rb
@@ -4,4 +4,9 @@ class FileUpload < ApplicationRecord
   # Validates
   validates :description, presence: true, uniqueness: { case_sensitive: false }
   has_many_attached :files
+
+  belongs_to :parent, class_name: 'FileUpload', optional: true
+
+  has_many :sub_directories, class_name: 'FileUpload',
+                             foreign_key: :parent_id, dependent: :destroy
 end
