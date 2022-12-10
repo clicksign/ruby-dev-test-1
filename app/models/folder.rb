5bed744392fc0325d50b4ed710c3f72a179a5890
lpaivareis <lucasfelipedepaiva2@gmail.com>
2022-12-10 18:24:58 -0300

    Add folders(migration, model, factory and spec file)

---
+++ app/models/folder.rb
@@ -0,0 +1,15 @@
+class Folder < ApplicationRecord
+  belongs_to :parent, class_name: "Folder", optional: true
+  has_many :children, class_name: "Folder", foreign_key: "parent_id", dependent: :destroy, inverse_of: :parent
+
+  has_many_attached :documents, dependent: :destroy
+
+  validates :name, presence: true
+  validates :parent, presence: true, if: :parent?
+
+  private
+
+  def parent?
+    parent.present?
+  end
+end
---
+++ spec/factories/folders.rb
@@ -0,0 +1,4 @@
+FactoryBot.define do
+  factory :folder do
+  end
+end
