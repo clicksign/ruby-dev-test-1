c64622a8111b3d77d0a392a119b10468e7dcceee
lpaivareis <lucasfelipedepaiva2@gmail.com>
2022-12-10 18:32:00 -0300

    Change folder_spec

--- spec/models/folder_spec.rb
+++ spec/models/folder_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Folder, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { is_expected.to have_many_attached(:documents) }
+    it { is_expected.to belong_to(:parent).class_name("Folder").optional }
+    it { is_expected.to have_many(:children).class_name("Folder").with_foreign_key("parent_id").dependent(:destroy) }
+  end
+
+  describe "validations" do
+    it "When name is present" do
+      folder = build(:folder)
+      expect(folder).to be_valid
+    end
+
+    it "When name is not present" do
+      folder = build(:folder, name: nil)
+      expect(folder).not_to be_valid
+    end
+
+    it "When parent is present" do
+      folder = build(:folder, parent: create(:folder))
+      expect(folder).to be_valid
+    end
+
+    it "When parent is not present" do
+      folder = build(:folder, parent: nil)
+      expect(folder).to be_valid
+    end
+  end
 end
