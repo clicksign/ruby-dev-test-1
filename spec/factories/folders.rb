75c2aba02e817b514c6c46651ab2823a1dc95644
lpaivareis <lucasfelipedepaiva2@gmail.com>
2022-12-10 18:27:28 -0300

    Change folder factory

--- spec/factories/folders.rb
+++ spec/factories/folders.rb
@@ -1,4 +1,15 @@
 FactoryBot.define do
   factory :folder do
+    name { Faker::Lorem.word }
+    parent_id { 1 }
+
+    trait :with_documents do
+      documents do
+        [
+          Rack::Test::UploadedFile.new(Rails.root.join("spec/fixtures/FakePDF.pdf"), "application/pdf"),
+          Rack::Test::UploadedFile.new(Rails.root.join("spec/fixtures/FakePDF2.pdf"), "application/pdf")
+        ]
+      end
+    end
   end
 end
