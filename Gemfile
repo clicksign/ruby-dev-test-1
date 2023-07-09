0c6a7f51e93ede90837ec5c5099d2ac95409f589
Gabriel Lovison <gabriel.lovison@gmail.com>
2023-07-09 11:36:17 -0300

    Add DotEnv gem and setup database with Postgres

--- Gemfile
+++ Gemfile
@@ -51,6 +51,7 @@ gem "bootsnap", require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
+  gem "dotenv", "~> 2.8.1"
   gem "factory_bot_rails", "~> 6.2.0"
   gem "faker", "~> 3.2.0"
   gem "rspec-rails", "~> 6.0.3"
--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,9 @@ module RubyDevTest1
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Brasilia"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.active_record.schema_format = :sql
   end
 end
