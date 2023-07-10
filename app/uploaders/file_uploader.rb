65cf9e961cdbb48eac89eb7921efd938a6c4df5c
Gabriel Lima <gabriellimatrajano@hotmail.com>
2023-07-10 18:05:37 -0300

    adding files in gitignore and comments

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -5,4 +5,9 @@ class FileUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.folder.full_path}/"
   end
+
+  # Example
+  # def extension_white_list
+  #   %w(pdf txt jpeg jpg png doc docx xls xlsx)
+  # end
 end
